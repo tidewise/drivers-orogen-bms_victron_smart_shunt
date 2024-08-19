55c282d14a4fcea577aa1c82a99d492c411e37cc
seixasxbr <mateus.seixas@tidewise.io>
2024-08-19 10:32:52 -0300

    initial commit

---
+++ bms_victron_smart_shuntTypes.hpp
@@ -0,0 +1,15 @@
+#ifndef bms_victron_smart_shunt_TYPES_HPP
+#define bms_victron_smart_shunt_TYPES_HPP
+
+/* If you need to define types specific to your oroGen components, define them
+ * here. Required headers must be included explicitly
+ *
+ * However, it is common that you will only import types from your library, in
+ * which case you do not need this file
+ */
+
+namespace bms_victron_smart_shunt {
+}
+
+#endif
+
